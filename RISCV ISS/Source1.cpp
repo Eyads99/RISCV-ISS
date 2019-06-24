81609406a6beba983ef7d317a6be1f8a0e543e00
Eyads99 <eyadshaban99@gmail.com>
2019-06-24 18:25:31 +0200

    formatting

--- RISCV ISS/Source1.cpp
+++ RISCV ISS/Source1.cpp
@@ -5,7 +5,7 @@
 
 	References:
 	(1) The risc-v ISA Manual ver. 2.1 @ https://riscv.org/specifications/
-	(2) https://github.com/michaeljclark/riscv-meta/blob/master/meta/opcodes
+	(2) https://github.com/michaeljclark/riscv-meta/blob/master/meta/opcodes 
 */
 
 #include <iostream>
